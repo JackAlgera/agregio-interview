301e2902375453f3fc0e6c1c09db55d6d7206ff5
JackAlgera <jack.algera@live.com>
2024-02-02 16:24:57 +0100

    chore(init): init Spring with Spring initializr

---
+++ src/main/java/jack/algera/agregio/reserves/ReservesApplication.java
@@ -0,0 +1,13 @@
+package jack.algera.agregio.reserves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservesApplication.class, args);
+	}
+
+}
