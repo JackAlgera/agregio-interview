267278a0af5e1c0ac90ae019e9952b10f5995ec8
JackAlgera <jack.algera@live.com>
2024-02-04 11:52:51 +0100

    style(application): lint the ReservesApplication and silly add unit test

--- src/main/java/jack/algera/agregio/reserves/ReservesApplication.java
+++ src/main/java/jack/algera/agregio/reserves/ReservesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReservesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReservesApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ReservesApplication.class, args);
+  }
 
 }
