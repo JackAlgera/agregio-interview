a0bf18983acfeaa9eab030534adb4670ed83bfee
JackAlgera <jack.algera@live.com>
2024-02-04 11:43:06 +0100

    feat(domain): implement offerService to make tests work

--- src/main/java/jack/algera/agregio/reserves/domain/adapters/OfferServiceImpl.java
+++ src/main/java/jack/algera/agregio/reserves/domain/adapters/OfferServiceImpl.java
@@ -1,7 +1,9 @@
 package jack.algera.agregio.reserves.domain.adapters;
 
 import java.util.List;
+import java.util.UUID;
 import jack.algera.agregio.reserves.domain.daos.OfferBlockDto;
+import jack.algera.agregio.reserves.domain.mappers.OfferBlockMapper;
 import jack.algera.agregio.reserves.domain.models.Market;
 import jack.algera.agregio.reserves.domain.models.Offer;
 import jack.algera.agregio.reserves.domain.ports.OfferRepository;
@@ -15,11 +17,19 @@ public class OfferServiceImpl implements OfferService {
 
   @Override
   public Offer createOffer(Market market, List<OfferBlockDto> offerBlocks) {
-    return null;
+    Offer offer = Offer.builder()
+        .id(UUID.randomUUID())
+        .market(market)
+        .offerBlocks(offerBlocks.stream()
+            .map(OfferBlockMapper::fromDto)
+            .toList())
+        .build();
+    offerRepository.save(offer);
+    return offer;
   }
 
   @Override
   public List<Offer> getOffersForMarket(Market market) {
-    return null;
+    return offerRepository.findAllByMarket(market);
   }
 }
